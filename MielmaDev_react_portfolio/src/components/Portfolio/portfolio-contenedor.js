0f17d61db53e8b4576f1690361a8d72b4f8ba5b1
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-11 12:29:56 +0200

    Descripción general del constructor de React

--- MielmaDev_react_portfolio/src/components/Portfolio/portfolio-contenedor.js
+++ MielmaDev_react_portfolio/src/components/Portfolio/portfolio-contenedor.js
@@ -1,18 +1,26 @@
 // importar react y componentes
 import React, { Component } from "react";
 // importar PortfolioItems creado
-import PortfolioItems from './portfolio-items';
+import PortfolioItem from "./portfolio-item";
 
 // Exportar predeterminada, clase, extender a componentes.
 // render sin argumento
 // Declaración return, parece html pero esl JavaScript(JSX)
 export default class PortfolioContainer extends Component {
+    constructor() {
+        // super() es el puente que conecta un componente de clase secundaria
+        // con su constructor de clase principal, permitir que la clase 
+        // secundaria herede todas las bondades del padre.
+        super();
+        console.log("El contenedor portfolio ha sido renderizado");
+    }
     render() {
         return (
             <div>
                 <h2>Este es el contenedor de portfolio</h2>
-                <PortfolioItems/>
+                <PortfolioItem/>
             </div>
         );
     }
 }
+
