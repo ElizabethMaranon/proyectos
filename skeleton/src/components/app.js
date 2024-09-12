e544d8d3a06c9c52d495773ea694edfd83250342
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-12 13:32:43 +0200

    React_Skeleton_Chrome_02

--- skeleton/src/components/app.js
+++ skeleton/src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ListaRevista from './revista/lista_revista';
 
 export default class App extends Component {
   render() {
@@ -6,7 +7,9 @@ export default class App extends Component {
       <div className='app'>
         <h1>Skeleton</h1>
         <h2>React, Props and State</h2>
+        <ListaRevista />
       </div>
     );
   }
 }
+
