41985c0c5f0d4f12cf6ee79860d5267aaca02a48
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-18 00:50:26 +0200

    Cómo cambiar los valores de estado en React🔗

--- Portfolio/src/components/portfolio/portfolio-contenedor.js
+++ Portfolio/src/components/portfolio/portfolio-contenedor.js
@@ -7,31 +7,42 @@ export default class PortfolioContenedor extends Component {
         this.state = {
             tituloPag: "Bienvenido a mi portfolio",
             datos: [
-                {titulo: "bat"},
-                {titulo: "bi"},
-                {titulo: "hiru"},
-                {titulo: "lau"},
+                { titulo: "bat", category: "eComercio" },
+                { titulo: "bi", category:  "Programación" },
+                { titulo: "hiru", category:  "Empresa" },
+                { titulo: "lau", category:  "eComercio" },
             ]
         };
-        this.manejarActPag = this.manejarActPag.bind(this);
+        this.manejarFiltro = this.manejarFiltro.bind(this);
+    }
+    manejarFiltro(filtro) {
+        this.setState({
+            datos: this.state.datos.filtro(item => {
+                return item.category === filtro;
+            })
+        });
     }
     portfolioItems() {
         return this.state.datos.map(item => {
             return <PortfolioItem titulo={item.titulo} url={"google.com"} />
         });
     }
-    manejarActPag() {
-        this.setState({
-            tituloPag: "El título ha sido cambiado"
-        });
-    }
+    // manejarActPag() {
+    //     this.setState({
+    //         tituloPag: "El título ha sido cambiado"
+    //     });
+    // }
     render() {
         return (
             <div>
                 <h2>{this.state.tituloPag}</h2>
+                <button onClick={() => this.manejarFiltro("eComercio")}>eComercio</button>
+                <button onClick={() => this.manejarFiltro("Programación")}>Programación</button>
+                <button onClick={() => this.manejarFiltro("Empresa")}>Empresa</button>
+
                 {this.portfolioItems()}
-                <hr />
-                <button onClick={this.manejarActPag}>Cambiar título</button>
+                {/* <hr />
+                <button onClick={this.manejarActPag}>Cambiar título</button> */}
             </div>
         );
     }
