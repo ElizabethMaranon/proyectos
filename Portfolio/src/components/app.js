823374c2a15f3c8bfe3806a61576cc7c45ef5bb5
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-19 22:58:30 +0200

    Revert "Configuración básica de ruta en React(Basic route configuration in React)"
    This reverts commit fa00cd13c2e6849a47a36d89d1c28850032660df.

--- Portfolio/src/components/app.js
+++ Portfolio/src/components/app.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
 import moment from "moment";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import PortfolioContenedor from './portfolio/portfolio-contenedor';
 import NavegarContenedor from './navegar/navegar-contenedor';
-import Home from './pag/home';
-import Sobre from './pag/sobre';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <Router>
-          <div>
-            <NavegarContenedor />
-            <Switch>
-              <Route exact path="/" Component={Home} />
-              <Route path="/Sobre" Component={Sobre} />
-            </Switch>
-          </div>
-        </Router>
+        <NavegarContenedor />
         <h1>MielmaDev Portfolio</h1>
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContenedor />
