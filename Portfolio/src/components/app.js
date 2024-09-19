fa00cd13c2e6849a47a36d89d1c28850032660df
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-19 12:32:14 +0200

    Configuración básica de ruta en React(Basic route configuration in React)

--- Portfolio/src/components/app.js
+++ Portfolio/src/components/app.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import PortfolioContenedor from './portfolio/portfolio-contenedor';
 import NavegarContenedor from './navegar/navegar-contenedor';
+import Home from './pag/home';
+import Sobre from './pag/sobre';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <NavegarContenedor />
+        <Router>
+          <div>
+            <NavegarContenedor />
+            <Switch>
+              <Route exact path="/" Component={Home} />
+              <Route path="/Sobre" Component={Sobre} />
+            </Switch>
+          </div>
+        </Router>
         <h1>MielmaDev Portfolio</h1>
         <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContenedor />
