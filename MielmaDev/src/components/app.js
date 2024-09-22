5ae9cf29f15b3befa0d177a5a6fbdec508140239
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-22 16:06:10 +0200

    Introducción a los componentes de React

--- MielmaDev/src/components/app.js
+++ MielmaDev/src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioCont from './portfolioCont';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>MielmaDev`s Portfolio</h1>
         <h2>{moment().format('MMMM Do YYYY, h:mm:ss a')}</h2>
+        <PortfolioCont />
       </div>
     );
   }
