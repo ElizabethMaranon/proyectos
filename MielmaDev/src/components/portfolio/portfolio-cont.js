4b5449b247c85583d3ecf1abf19147f83c7cf8e5
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-22 16:14:15 +0200

    Descripción general del constructor de reacción

--- MielmaDev/src/components/portfolio/portfolio-cont.js
+++ MielmaDev/src/components/portfolio/portfolio-cont.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioCont extends Component {
+  constructor(){
+    super();
+    console.log("portfolio renderizado");
+  }
   render() {
     return (
       <div>
