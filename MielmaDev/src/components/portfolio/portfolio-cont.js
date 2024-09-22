7cd7a36e50500b8bef8f3d1d0cdcb2b0fc8bda61
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-22 16:16:46 +0200

    Funciones personalizadas en React y uso de Map para recorrer datos

--- MielmaDev/src/components/portfolio/portfolio-cont.js
+++ MielmaDev/src/components/portfolio/portfolio-cont.js
@@ -6,11 +6,17 @@ export default class PortfolioCont extends Component {
     super();
     console.log("portfolio renderizado");
   }
+  portfolioItems() {
+    const datos = [1,2,3,4];
+    return datos.map(item => {
+      return <PortfolioItem/>;
+    });
+  }
   render() {
     return (
       <div>
         <h2>Portfolio items go here updated...</h2>
-        <PortfolioItem />
+        {this.portfolioItems()}
       </div>
     );
   }
