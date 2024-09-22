e0ffadb57fe07b9f08aab1f8c5d4a1cabcc2559d
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-22 16:53:57 +0200

    Cómo utilizar operadores ternarios en código JSX para implementar un comportamiento condicional

--- MielmaDev/src/components/app.js
+++ MielmaDev/src/components/app.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import moment from "moment";
 
 import PortfolioCont from './portfolio/portfolio-Cont';
+import NavCont from './nav/nav-cont';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavCont />
         <h1>MielmaDev`s Portfolio</h1>
         <h2>{moment().format('MMMM Do YYYY, h:mm:ss a')}</h2>
         <PortfolioCont />
