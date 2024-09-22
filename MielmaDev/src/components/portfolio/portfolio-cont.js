2cbc60a5ac0e2f006b0721ec13bfb54a93cc5fcf
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-22 16:21:15 +0200

    Descripción general de los accesorios en React

--- MielmaDev/src/components/portfolio/portfolio-cont.js
+++ MielmaDev/src/components/portfolio/portfolio-cont.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
+
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioCont extends Component {
   constructor(){
     super();
+
     console.log("portfolio renderizado");
   }
+
   portfolioItems() {
     const datos = [1,2,3,4];
+
     return datos.map(item => {
-      return <PortfolioItem/>;
+      return <PortfolioItem titulo={item} url={"mielmadev.com"} />;
     });
   }
+  
   render() {
     return (
       <div>
         <h2>Portfolio items go here updated...</h2>
+
         {this.portfolioItems()}
       </div>
     );
