088d8d556e24f79a1d1addf06cd9995137d74ad9
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-22 16:31:51 +0200

    Introducción al trabajo con el estado en React

--- MielmaDev/src/components/portfolio/portfolio-cont.js
+++ MielmaDev/src/components/portfolio/portfolio-cont.js
@@ -6,21 +6,28 @@ export default class PortfolioCont extends Component {
   constructor(){
     super();
 
-    console.log("portfolio renderizado");
+    this.state = {
+      tituloPag:"Bienvenido al portfolio de MielmaDev",
+      datos: [
+        {titulo: 1 },
+        {titulo: 2 },
+        {titulo: 3 },
+        {titulo: 4 },
+      ]
+
+    }
   }
 
   portfolioItems() {
-    const datos = [1,2,3,4];
-
-    return datos.map(item => {
-      return <PortfolioItem titulo={item} url={"mielmadev.com"} />;
+    return this.state.datos.map(item => {
+      return <PortfolioItem titulo={item.titulo} url={"mielmadev.com"} />;
     });
   }
   
   render() {
     return (
       <div>
-        <h2>Portfolio items go here updated...</h2>
+        <h2>{this.state.tituloPag}</h2>
 
         {this.portfolioItems()}
       </div>
