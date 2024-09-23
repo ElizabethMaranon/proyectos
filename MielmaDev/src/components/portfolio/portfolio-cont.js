2c09a7f84de4d8bb5cfc8a235e6acbacae753865
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-23 20:50:37 +0200

    Cómo renderizar datos API en React

--- MielmaDev/src/components/portfolio/portfolio-cont.js
+++ MielmaDev/src/components/portfolio/portfolio-cont.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioCont extends Component {
@@ -9,21 +10,12 @@ export default class PortfolioCont extends Component {
     this.state = {
       tituloPag: "Bienvenido al portfolio de MielmaDev",
       cargando: false,
-      datos: [
-        { titulo: 1, category: "eCommerce", slug: "bat" },
-        { titulo: 2, category: "Programación", slug: "bi" },
-        {
-          titulo: 3,
-          category: "Empresa",
-          slug: "hiru"
-        },
-        { titulo: 4, category: "eCommerce", slug: "lau" },
-      ]
+      datos: []
     };
 
     this.manejarFiltro = this.manejarFiltro.bind(this);
-    this.getPortfolioItems = this.getPortfolioItems.bind(this);
   }
+
   manejarFiltro(filtro) {
     this.setState({
       datos: this.state.datos.filtro(item => {
@@ -31,31 +23,39 @@ export default class PortfolioCont extends Component {
       })
     });
   }
+
   getPortfolioItems() {
     axios
       .get("https://mielmadev.devcamp.space/portfolio/portfolio_items")
-      .then(response => {
-        console.log("datos de respuesta", response);
+      .then(respuesta => {
+        this.setState({
+          datos: respuesta.data.portfolio_items
+        });
       })
       .catch(error => {
         console.log(error);
       });
   }
+
   portfolioItems() {
     return this.state.datos.map(item => {
-      return (
-        <PortfolioItem titulo={item.titulo} url={"mielmadev.com"} slug={item.slug} />
-      )
+      return <PortfolioItem titulo={item.name} url={item.url} slug={item.id} />
     });
   }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     if (this.state.cargando) {
       return <div>El portfolio se está cargando, un momento por favor</div>
     }
-    this.getPortfolioItems();
+
     return (
       <div>
         <h2>{this.state.tituloPag}</h2>
+        
         <button onClick={() => this.manejarFiltro("eCommerce")}>
           eCommerce
         </button>
@@ -65,6 +65,7 @@ export default class PortfolioCont extends Component {
         <button onClick={() => this.manejarFiltro("Enterprise")}>
           Enterprise
         </button>
+
         {this.portfolioItems()}
       </div>
     );
