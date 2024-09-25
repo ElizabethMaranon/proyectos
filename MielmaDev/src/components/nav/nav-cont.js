1d5f461dbe5fdced7bb0a7be3b2c38566714640e
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-25 21:27:35 +0200

    Creación del contenedor Flexbox para el componente de navegación

--- MielmaDev/src/components/nav/nav-cont.js
+++ MielmaDev/src/components/nav/nav-cont.js
@@ -8,25 +8,27 @@ export default class NavCont extends Component {
 
     render() {
         return (
-            <div>
-                <NavLink exact to="/" activeClassName="nav-link-active">
-                    Home
-                </NavLink>
+            <div className="nav-paq">
+                <div className="lado-izq">
+                    <NavLink exact to="/" activeClassName="nav-link-active">
+                        Home
+                    </NavLink>
 
-                <NavLink to="/about-me" activeClassName="nav-link-active">
-                    About
-                </NavLink>
+                    <NavLink to="/about-me" activeClassName="nav-link-active">
+                        About
+                    </NavLink>
+                    <NavLink to="/contacto" activeClassName="nav-link-active">
+                        Contacto
+                    </NavLink>
+                    <NavLink to="/blog" activeClassName="nav-link-active">
+                        Blog
+                    </NavLink>
 
-                <NavLink to="/contacto" activeClassName="nav-link-active">
-                    Contacto
-                </NavLink>
-
-                <NavLink to="/blog" activeClassName="nav-link-active">
-                    Blog
-                </NavLink>
-
-                {false ? <button>Add Blog</button> : null}
+                    {false ? <button>Add Blog</button> : null}
+                </div>
+                <div className="lado-dcho">MielmaDev</div>
             </div>
         );
     }
 }
+
