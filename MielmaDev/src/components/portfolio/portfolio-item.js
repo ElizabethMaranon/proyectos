ffb2ac42baab7725b1e26f96cba63b1dd8ced496
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-09-26 20:37:55 +0200

    Cómo usar estilos en línea en React para renderizar imágenes de fondo

--- MielmaDev/src/components/portfolio/portfolio-item.js
+++ MielmaDev/src/components/portfolio/portfolio-item.js
@@ -5,7 +5,12 @@ export default function (props) {
   const { id, description, thumb_image_url, logo_url } = props.item;
   return (
     <div className="portfolio-item-paq">
-      <img src={thumb_image_url} />
+      <div
+        className="portfolio-img-fondo"
+        style={{
+          backgroundImage: "url(" + thumb_image_url + ")"
+        }}
+      />
       <img src={logo_url} />
       <div>{description}</div>
       <Link to={`/portfolio/${id}`}>Link</Link>
