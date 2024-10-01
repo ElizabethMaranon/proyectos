d8c3a652480a705a1a3ca2c62c7369cfb56447fb
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-10-01 21:24:42 +0200

    Construyendo una guardia de ruta en React

--- MielmaDev/src/components/app.js
+++ MielmaDev/src/components/app.js
@@ -65,12 +65,16 @@ export default class App extends Component {
     this.checkLoginStatus();
   }
 
+  authorizedPages() {
+    return [<Route path="/blog" component={Blog} />];
+  }
+
   render() {
     return (
       <div className="container">
         <Router>
           <div>
-          <NavigationContainer loggedInStatus={this.state.loggedInStatus} />
+            <NavigationContainer loggedInStatus={this.state.loggedInStatus} />
 
             <h2>{this.state.loggedInStatus}</h2>
 
@@ -90,8 +94,9 @@ export default class App extends Component {
 
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
-              <Route
+              {this.state.loggedInStatus === "LOGGED_IN" ? (
+                this.authorizedPages()
+              ) : null}              <Route
                 exact
                 path="/portfolio/:slug"
                 component={PortfolioDetail}
