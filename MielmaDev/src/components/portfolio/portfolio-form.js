aab3386e6a1031556eefbe1916cc33dcf240bf31
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-10-03 17:52:04 +0200

    Crear el formulario de cartera y llamarlo desde el componente principal

---
+++ MielmaDev/src/components/portfolio/portfolio-form.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+export default class PortfolioForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>formulario 1</h1>
+        <h1>formulario 2</h1>
+      </div>
+    );
+  }
+}
