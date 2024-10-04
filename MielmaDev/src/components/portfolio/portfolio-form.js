1365c145efeecd627b707896f6c95319144b6c3a
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-10-04 18:33:07 +0200

    Guía para seleccionar menús desplegables en React

--- MielmaDev/src/components/portfolio/portfolio-form.js
+++ MielmaDev/src/components/portfolio/portfolio-form.js
@@ -40,18 +40,18 @@ export default class PortfolioForm extends Component {
 
   handleSubmit(event) {
     axios
-    .post(
+      .post(
 
-      "https://mielmadev.devcamp.space/portfolio/portfolio_items",
-      this.buildForm(),
-      { withCredentials: true }
-    )
-    .then(response => {
-      console.log("response", response);
-    })
-    .catch(error => {
-      console.log("portfolio form handleSubmit error", error);
-    });
+        "https://mielmadev.devcamp.space/portfolio/portfolio_items",
+        this.buildForm(),
+        { withCredentials: true }
+      )
+      .then(response => {
+        console.log("response", response);
+      })
+      .catch(error => {
+        console.log("portfolio form handleSubmit error", error);
+      });
     event.preventDefault();
   }
 
@@ -88,14 +88,15 @@ export default class PortfolioForm extends Component {
               onChange={this.handleChange}
             />
 
-            <input
-              type="text"
+            <select
               name="category"
-              placeholder="Category"
               value={this.state.category}
               onChange={this.handleChange}
-            />
-          </div>
+            >
+              <option value="eCommerce">eCommerce</option>
+              <option value="Scheduling">Scheduling</option>
+              <option value="Enterprise">Enterprise</option>
+            </select>          </div>
 
           <div>
             <input
