6444af959adafe79b25ac40c0b27e809915c9930
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-10-06 19:01:07 +0200

    Cómo diseñar iconos de fuentes impresionantes en React

--- MielmaDev/src/components/portfolio/portfolio-sidebar-list.js
+++ MielmaDev/src/components/portfolio/portfolio-sidebar-list.js
@@ -8,15 +8,21 @@ const PortfolioSidebarList = props => {
         <div className="portfolio-thumb-img">
           <img src={portfolioItem.thumb_image_url} />
         </div>
-        <h1 className="title">{portfolioItem.name}</h1>
-        <h2>{portfolioItem.id}</h2>
-        <a onClick={() => props.handleDeleteClick(portfolioItem)}>
+
+        <div className="text-content">
+          <div className="title">{portfolioItem.name}</div>
+          <a
+            className="delete-icon"
+            onClick={() => props.handleDeleteClick(portfolioItem)}
+          >
           <FontAwesomeIcon icon="trash" />
         </a>
+        </div>
       </div>
+
     );
   });
 
-  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
+return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
 };
 export default PortfolioSidebarList;
