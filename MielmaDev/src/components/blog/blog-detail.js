634d28c4297f8d5d899afcab9c60cf5d276e3f6e
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-10-08 20:17:38 +0200

    Creación del componente de detalles del blog inicial

---
+++ MielmaDev/src/components/blog/blog-detail.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"; // importar react component
+
+export default class BlogDetail extends Component { // Class Component
+  render() {
+    return (
+      <div>
+        <h1>Blog detail</h1>
+      </div>
+    );
+  }
+}
