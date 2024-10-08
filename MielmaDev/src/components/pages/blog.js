82b29dde34e3135b2b768d5354e7abd482a37a40
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-10-08 15:51:44 +0200

    Conversión del componente de blog funcional en un componente de clase

--- MielmaDev/src/components/pages/blog.js
+++ MielmaDev/src/components/pages/blog.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { Component } from "react"; // importar componente react
 import { Link } from "react-router-dom";
 
-export default function() {
-  return (
-    <div>
-      <h2>Blog</h2>
+class Blog extends Component { // Agregar Class
+  constructor() { // Obligatorio Constructor
+    super(); // Obligatorio Super en constructor
+  }
 
+  render() { // Obligatorio class component
+    return ( // Devuelve
       <div>
-        <Link to="/about-me">Read more about myself</Link>
+        <h2>Blog</h2>
+        <div>
+          <Link to="/about-me">Read more about myself</Link> {/*Enlace insertado*/}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+export default Blog; // exportar class componente
