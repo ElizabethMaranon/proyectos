fd5daeaaf7e9f010862204ef745c3ef6433007a9
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-10-08 21:54:49 +0200

    Representar los detalles del blog en la pantalla

--- MielmaDev/src/components/blog/blog-detail.js
+++ MielmaDev/src/components/blog/blog-detail.js
@@ -4,8 +4,9 @@ import axios from "axios"; // Conectar API
 export default class BlogDetail extends Component { // Class Component
   constructor(props) { // llamar constructor con accesorios porque necesitamos poder acceder al parámetro de ruta
     super(props); // Super obligatorio por constructor
+
     this.state = { // representa estado actual id {// método que utiliza para actualizar}
-      currentId: this.props.match.params.slug, 
+      currentId: this.props.match.params.slug,
       blogItem: {} // comenzará como un objeto vacío, 
     };
   }
@@ -17,7 +18,9 @@ export default class BlogDetail extends Component { // Class Component
           .currentId}`
       )
       .then(response => { // Cuando se resuelva la promesa
-        console.log("response", response); // probar
+        this.setState({ // configurar blogItem
+          blogItem: response.data.portfolio_blog
+        });
       })
       .catch(error => {  // detector de error
         console.log("getBlogItem error", error);
@@ -29,9 +32,17 @@ export default class BlogDetail extends Component { // Class Component
   }
 
   render() { // Obligatorio class component 
+    const { // Atributos (desestructuración palabras clave)
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
     return (
       <div>
-        <h1>Blog detail</h1>
+        <h1>{title}</h1> {/*Obtener título */}
+        <img src={featured_image_url} />{/* Obtener imagen sin estilo */}
+        <div>{content}</div> {/*Obtener contenido */}
       </div>
     );
   }
