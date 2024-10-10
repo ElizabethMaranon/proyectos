a8ea46e6b191c064cfb8d14354ee67e80bce4964
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-10-10 15:22:15 +0200

    Aplicar estilos personalizados al modal React

--- MielmaDev/src/components/modals/blog-modal.js
+++ MielmaDev/src/components/modals/blog-modal.js
@@ -4,11 +4,26 @@ import ReactModal from "react-modal";
 export default class BlogModal extends Component { // componente clase xq tiene su propio estado
   constructor(props) { // constructor básico con accesorios
     super(props); // llamará a super
+
+    this.customStyles = { // agregar estilos modal
+      content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%",
+        width: "800px"
+      },
+      overlay: { // superposición
+        backgroundColor: "rgba(238, 202, 85, 0.75)" // color superposición (0,75 es el grado de traslucido)
+      }
+    };
   }
 
   render() { // función render obligatoria para class component
     return ( // devuelve
       <ReactModal
+                style={this.customStyles} // llamar al estilo
         onRequestClose={() => { // accesorio onRequestClose
           this.props.handleModalClose(); // Aplicar componente
         }}
