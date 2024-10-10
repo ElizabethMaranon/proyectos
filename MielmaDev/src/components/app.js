2b88bcac5a5cf7417c26e38c5d79d95c5b527099
ElizabethMaranon <elizabethmaranon80@gmail.com>
2024-10-10 20:41:52 +0200

    Creación de un archivo auxiliar de iconos dedicado en React

--- MielmaDev/src/components/app.js
+++ MielmaDev/src/components/app.js
@@ -1,16 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
-import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTrash, // icono papelera
-  faSignOutAlt, // icono cerrar sesión
-  faEdit, // Icono editar
-  faSpinner, // icono loading
-  faPlusCircle, // Plus en un círculo
-} from "@fortawesome/free-solid-svg-icons";
-
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -21,12 +12,13 @@ import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
-
-library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle); // agregar iconos a la biblioteca (mala practica enumerarlos, mas tarde arreglarlo con función auxiliar)
+import Icons from "../helpers/icons";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    Icons(); // llamar función desde constructor
 
     this.state = {
       loggedInStatus: "NOT_LOGGED_IN"
